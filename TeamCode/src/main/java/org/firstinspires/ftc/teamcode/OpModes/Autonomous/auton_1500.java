f3eb3f537aa76c488ca990f2c6db225aa0797eb6
XJ <91567294+XJ1729@users.noreply.github.com>
2024-01-18 13:13:19 -0800

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/auton_1500.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.hardware.Claw;
+import org.firstinspires.ftc.teamcode.hardware.DrivePower;
+
+// @Disabled
+@Autonomous(name="auton_1500", group="Linear OpMode")
+
+public class auton_1500 extends LinearOpMode{
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    DrivePower drive;
+    Claw claw;
+
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Intialized");
+        telemetry.update();
+
+        claw = new Claw(hardwareMap);
+        claw.openClaw();
+
+        drive = new DrivePower(hardwareMap);
+        // drive.resetEncoders();
+
+        waitForStart();
+        runtime.reset();
+
+        // write code here using functions
+        // claw.closeClaw();
+        drive.forward(0.5);
+        sleep(1000);
+        drive.stop();
+        // claw.openClaw();
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/basic_auton.java
@@ -0,0 +1,231 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+// version 1 of team taro's autonomous code.
+@Disabled
+@Autonomous(name="basic_auton", group="Linear OpMode")
+
+public class basic_auton extends LinearOpMode{
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor fldrive, frdrive, bldrive, brdrive;
+
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Intialized");
+        telemetry.update();
+
+        fldrive  = hardwareMap.get(DcMotor.class, "fldrive");
+        frdrive  = hardwareMap.get(DcMotor.class, "frdrive");
+        brdrive = hardwareMap.get(DcMotor.class, "brdrive");
+        bldrive = hardwareMap.get(DcMotor.class, "bldrive");
+
+        fldrive.setDirection(DcMotor.Direction.FORWARD);
+        frdrive.setDirection(DcMotor.Direction.REVERSE);
+        brdrive.setDirection(DcMotor.Direction.REVERSE);
+        bldrive.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        runtime.reset();
+
+        //write code here using functions
+        forward(0.1, 2000);
+
+    }
+
+
+    public void forward(double power, int distance) {
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fldrive.setTargetPosition(distance);
+        frdrive.setTargetPosition(distance);
+        bldrive.setTargetPosition(distance);
+        brdrive.setTargetPosition(distance);
+
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fldrive.setPower(power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(power);
+
+        while (fldrive.isBusy() && frdrive.isBusy() && bldrive.isBusy() && brdrive.isBusy()) {
+            //until point reached
+        }
+
+        power = 0.0;
+        fldrive.setPower(power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(power);
+    }
+
+    public void backward(double power, int distance) {
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fldrive.setTargetPosition(-distance);
+        frdrive.setTargetPosition(-distance);
+        bldrive.setTargetPosition(-distance);
+        brdrive.setTargetPosition(-distance);
+
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fldrive.setPower(-power);
+        frdrive.setPower(-power);
+        brdrive.setPower(-power);
+        bldrive.setPower(-power);
+
+        while (fldrive.isBusy() && frdrive.isBusy() && bldrive.isBusy() && brdrive.isBusy()) {
+            //until point reached
+        }
+
+        power = 0.0;
+        fldrive.setPower(-power);
+        frdrive.setPower(-power);
+        brdrive.setPower(-power);
+        bldrive.setPower(-power);
+    }
+
+    public void left(double power, int distance) {
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fldrive.setTargetPosition(-distance);
+        frdrive.setTargetPosition(distance);
+        bldrive.setTargetPosition(distance);
+        brdrive.setTargetPosition(-distance);
+
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fldrive.setPower(-power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(-power);
+
+        while (fldrive.isBusy() && frdrive.isBusy() && bldrive.isBusy() && brdrive.isBusy()) {
+            //until point reached
+        }
+
+        power = 0.0;
+        fldrive.setPower(power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(power);
+    }
+
+    public void right(double power, int distance) {
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fldrive.setTargetPosition(distance);
+        frdrive.setTargetPosition(-distance);
+        bldrive.setTargetPosition(-distance);
+        brdrive.setTargetPosition(distance);
+
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fldrive.setPower(power);
+        frdrive.setPower(-power);
+        brdrive.setPower(-power);
+        bldrive.setPower(power);
+
+        while (fldrive.isBusy() && frdrive.isBusy() && bldrive.isBusy() && brdrive.isBusy()) {
+            //until point reached
+        }
+
+        power = 0.0;
+        fldrive.setPower(-power);
+        frdrive.setPower(-power);
+        brdrive.setPower(-power);
+        bldrive.setPower(-power);
+    }
+    public void strafeleft(double power, int distance) {
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fldrive.setTargetPosition(-distance);
+        frdrive.setTargetPosition(distance);
+        bldrive.setTargetPosition(-distance);
+        brdrive.setTargetPosition(distance);
+
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fldrive.setPower(-power);
+        frdrive.setPower(power);
+        brdrive.setPower(-power);
+        bldrive.setPower(power);
+
+        while (fldrive.isBusy() && frdrive.isBusy() && bldrive.isBusy() && brdrive.isBusy()) {
+            //until point reached
+        }
+
+        power = 0.0;
+        fldrive.setPower(power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(power);
+    }
+    public void straferight(double power, int distance) {
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fldrive.setTargetPosition(distance);
+        frdrive.setTargetPosition(-distance);
+        bldrive.setTargetPosition(distance);
+        brdrive.setTargetPosition(-distance);
+
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fldrive.setPower(power);
+        frdrive.setPower(-power);
+        brdrive.setPower(power);
+        bldrive.setPower(-power);
+
+        while (fldrive.isBusy() && frdrive.isBusy() && bldrive.isBusy() && brdrive.isBusy()) {
+            //until point reached
+        }
+
+        power = 0.0;
+        fldrive.setPower(power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(power);
+    }
+}
